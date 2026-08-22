0c4958f5fc663daa99e45a03544af0df14e1c922
ihazahmed7789-blip <ihazahmed7789@gmail.com>
2026-08-22 07:52:01 +0500

    Delete backend directory

--- backend/routes/salaryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const ctrl = require("../controllers/salaryController");
-const { verifyToken, allowRoles } = require("../middleware/auth");
-
-router.get("/", verifyToken, allowRoles("admin"), ctrl.getPayments);
-router.post("/", verifyToken, allowRoles("admin"), ctrl.recordPayment);
-
-module.exports = router;
