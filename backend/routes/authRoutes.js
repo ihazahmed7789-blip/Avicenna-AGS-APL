6b95d71bb70cd11c8641c77c915c4a2ddba6b15e
ihazahmed7789-blip <ihazahmed7789@gmail.com>
2026-08-24 14:00:01 +0500

    Delete backend directory

--- backend/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { login, createUser } = require("../controllers/authController");
-const { verifyToken, allowRoles } = require("../middleware/auth");
-
-router.post("/login", login);
-router.post("/users", verifyToken, allowRoles("admin"), createUser);
-
-module.exports = router;
